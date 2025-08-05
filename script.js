21097b08bffe9d5af27a80d91c12c9781f247ee7
Lyquang <lythanhnhatquangthongnhat2004@gmail.com>
2025-08-05 14:47:03 +0700

    fix swiper product thumbs img

--- script.js
+++ script.js
@@ -43,113 +43,126 @@ window.addEventListener("load", function () {
       el: ".swiper-pagination",
       clickable: true,
     },
-    effect: "slide", // Có thể thay: "fade", "cube", "coverflow", "flip"
+    effect: "fade", // Có thể thay: "fade", "cube", "coverflow", "flip"
     speed: 1500,
   });
-});
 
+  // swiper for product
+  const thumbsSwiper = new Swiper(".product-swiper-thumbs", {
+    loop: true,
+    spaceBetween: 10,
+    slidesPerView: 4,
+    freeMode: true,
+    watchSlidesProgress: true,
+  });
 
-// Swiper cho mục Tin tức đào tạo
-const eduNewsSwiper = new Swiper(".edu-news-swiper", {
-  slidesPerView: 4,
-  loop: true,
-  speed: 600,
-  navigation: {
-    nextEl: "#slideRight",
-    prevEl: "#slideLeft",
-  },
-  breakpoints: {
-    576: {
-      slidesPerView: 1,
+  const mainSwiper = new Swiper(".product-swiper-main", {
+    loop: true,
+    spaceBetween: 10,
+    thumbs: {
+      swiper: thumbsSwiper,
     },
-    768: {
-      slidesPerView: 2,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
     },
-    992: {
-      slidesPerView: 3,
-    },
-    1200: {
-      slidesPerView: 4,
-    },
-  },
-});
+  });
 
-// thong tin giao duc
-new Swiper(".myCourseSwiper", {
-  slidesPerView: 2,
-  spaceBetween: 30,
-  speed: 600,
-  loop: true,
-  navigation: {
-    nextEl: "#slideRight2",
-    prevEl: "#slideLeft2",
-  },
-  breakpoints: {
-    768: {
-      slidesPerView: 2,
+  // Swiper cho mục Tin tức đào tạo
+  const eduNewsSwiper = new Swiper(".edu-news-swiper", {
+    slidesPerView: 4,
+    loop: true,
+    speed: 600,
+    navigation: {
+      nextEl: "#slideRight",
+      prevEl: "#slideLeft",
     },
-    576: {
-      slidesPerView: 1,
+    breakpoints: {
+      576: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      992: {
+        slidesPerView: 3,
+      },
+      1200: {
+        slidesPerView: 4,
+      },
     },
-  }
-});
+  });
 
-
-
-
-
-
-// section news
-new Swiper(".myNewsSwiper", {
-  slidesPerView: 4,
-  spaceBetween: 24,
-  loop: true,
-  speed: 800,
-  autoplay: {
-    delay: 4000,
-    disableOnInteraction: false,
-  },
-  breakpoints: {
-    768: {
-      slidesPerView: 2,
+  // thong tin giao duc
+  new Swiper(".myCourseSwiper", {
+    slidesPerView: 2,
+    spaceBetween: 30,
+    speed: 600,
+    loop: true,
+    navigation: {
+      nextEl: "#slideRight2",
+      prevEl: "#slideLeft2",
     },
-    992: {
-      slidesPerView: 3,
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+      },
+      576: {
+        slidesPerView: 1,
+      },
     },
-    1200: {
-      slidesPerView: 4,
-    },
-  },
-});
+  });
 
+  // section news
+  new Swiper(".myNewsSwiper", {
+    slidesPerView: 4,
+    spaceBetween: 24,
+    loop: true,
+    speed: 800,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+      },
+      992: {
+        slidesPerView: 3,
+      },
+      1200: {
+        slidesPerView: 4,
+      },
+    },
+  });
 
+  // section SEpark
 
-// section SEpark
-
-new Swiper(".seParkSwiper", {
-  slidesPerView: 4,
-  spaceBetween: 40,
-  loop: true,
-  speed: 3000,
-  autoplay: {
-    delay: 0, // không có delay giữa các lần chuyển
-    disableOnInteraction: false,
-  },
-  freeMode: true, // giúp trượt mượt
-  breakpoints: {
-    576: {
-      slidesPerView: 3,
+  new Swiper(".seParkSwiper", {
+    slidesPerView: 4,
+    spaceBetween: 40,
+    loop: true,
+    speed: 3000,
+    autoplay: {
+      delay: 0, // không có delay giữa các lần chuyển
+      disableOnInteraction: false,
     },
-    768: {
-      slidesPerView: 4,
+    freeMode: true, // giúp trượt mượt
+    breakpoints: {
+      576: {
+        slidesPerView: 3,
+      },
+      768: {
+        slidesPerView: 4,
+      },
+      992: {
+        slidesPerView: 5,
+      },
+      1200: {
+        slidesPerView: 6,
+      },
     },
-    992: {
-      slidesPerView: 5,
-    },
-    1200: {
-      slidesPerView: 6,
-    },
-  },
+  });
 });
 
 /*========== scroll sections active link ==========*/
@@ -180,28 +193,28 @@ window.onscroll = () => {
 
 // document.getElementById("slideLeft").addEventListener("click", function () {
 //   document.getElementById("eduNewsSlider").scrollBy({
-//     left: -355, 
+//     left: -355,
 //     behavior: "smooth",
 //   });
 // });
 
 // document.getElementById("slideRight").addEventListener("click", function () {
 //   document.getElementById("eduNewsSlider").scrollBy({
-//     left: 355, 
+//     left: 355,
 //     behavior: "smooth",
 //   });
 // });
 
 // document.getElementById("slideLeft2").addEventListener("click", function () {
 //   document.getElementById("courseIT").scrollBy({
-//     left: -710, 
+//     left: -710,
 //     behavior: "smooth",
 //   });
 // });
 
 // document.getElementById("slideRight2").addEventListener("click", function () {
 //   document.getElementById("courseIT").scrollBy({
-//     left: 710, 
+//     left: 710,
 //     behavior: "smooth",
 //   });
 // });
